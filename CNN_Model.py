c51c6304a4e67efbffbc95d3799c1661df189d3b
Ranjan <ranjan.raut18@gmail.com>
2019-11-18 19:53:14 -0700

    Added Script for on-demand detection

--- CNN_Model.py
+++ CNN_Model.py
@@ -1,7 +1,5 @@
 import os
 
-import keras
-import keras_metrics
 from keras.layers import Conv1D, MaxPooling1D
 from keras.layers import Dense
 from keras.layers import Flatten, Dropout, Activation
@@ -29,14 +27,14 @@ class CNNModel:
         # self.__model.compile(loss='sparse_categorical_crossentropy', optimizer=opt,
         #                    metrics=['accuracy', keras_metrics.precision(), keras_metrics.recall(),
         #                             keras_metrics.f1_score()])
-        self.__model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
-                             # metrics=['accuracy', keras_metrics.precision(), keras_metrics.recall(),
-                             #          keras_metrics.f1_score()])
+        self.__model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        # metrics=['accuracy', keras_metrics.precision(), keras_metrics.recall(),
+        #          keras_metrics.f1_score()])
         self.__model.summary()
 
     def train_model(self, X_train, y_train, X_test, y_test, batch_size, epochs):
         training_details = self.__model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-                                          validation_data=(X_test, y_test))
+                                            validation_data=(X_test, y_test))
 
         return training_details
 
